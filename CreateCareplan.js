628e61e798cd15d7091735725620d5f16bdf761d
Jason <jason@care-planner.co.uk>
2021-05-28 16:59:03 +0100

    todo nonDev login

--- CreateCareplan.js
+++ CreateCareplan.js
@@ -18,32 +18,46 @@ process.argv.forEach(function (val, index, array) {
 });
 
 (async function main() {
-  let link = await oneTimeLogin.login();
-  await devTest(link);
-})();
-
-async function devTest(loginLink) {
   let driver;
   if (!isHeadless) {
     driver = await new webdriver.Builder()
-      .forBrowser('chrome')
-      .setChromeOptions()
-      .build();
+        .forBrowser('chrome')
+        .setChromeOptions()
+        .build();
   } else {
     driver = await new webdriver.Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(opts.headless())
-      .build();
+        .forBrowser('chrome')
+        .setChromeOptions(opts.headless())
+        .build();
   }
+  await driver.manage().window().setRect({ width: 1366, height: 768 });
+  if (isDev) {
+    let link = await oneTimeLogin.login();
+    await devLogin(link, driver);
+  } else {
+    await nonDevLogin(driver);
+  }
+  await test(driver);
+})();
 
+async function devLogin(loginLink, driver) {
   await driver.get(loginLink);
   if (await driver.getCurrentUrl() === 'http://localhost:8081/user/password') {
     await driver.quit();
     throw new Error('One time login failed');
   }
-  await driver.manage().window().setRect({ width: 1366, height: 768 });
+}
+
+async function nonDevLogin(driver) {
+  // todo: nonDev login with admin user
+}
+
+async function test(driver) {
   let clientsElement = await driver.findElement(webdriver.By.css('.menu-176'));
   await clientsElement.click();
+
+  // todo: create new user
+
 
   // let userInput = driver.findElement(webdriver.By.css('#edit-name'));
   // userInput.sendKeys('asdfasdfsafdasdf');
